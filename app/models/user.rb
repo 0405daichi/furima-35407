07f31b9aed790f2a44d3ef2c4ddb9ad07e940b8d
0405daichi <daichi.fukunaga.45@gmail.com>
2021-04-29 12:39:02 +0900

    ユーザー管理機能　修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,27 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :family_name, presence: true
-  validates :first_name, presence: true
-  validates :family_ruby, presence: true
-  validates :first_ruby, presence: true
-  validates :birthday, presence: true
-  validates :encrypted_password, presence: true
-
-  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+  with_options presence: true do      
+    validates :nickname
     validates :family_name
     validates :first_name
     validates :family_ruby
     validates :first_ruby
+    validates :birthday
   end
+
+  with_options presence: true, format: { with: /\A[ぁ-ゔァ-ヴ\p{Ideographic}ａ-ｚＡ-Ｚ０-９]+\z/, message: 'に全角文字を使用してください' } do
+    validates :family_name
+    validates :first_name
+  end
+
+  validates :name, presence: true, format: {with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい"} do
+    validates :family_ruby
+    validates :first_ruby
+  end
+
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
   has_many :items
   has_many :comments
