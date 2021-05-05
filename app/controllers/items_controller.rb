b3c3f840eb4ae20757b479112def39747d73d5fa
0405daichi <daichi.fukunaga.45@gmail.com>
2021-05-05 14:45:33 +0900

    商品出品機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,22 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :item_price, :burden_id, :source_id, :guideline_id, :image).merge(user_id: current_user.id)
+  end
 end
---
+++ app/models/item.rb
@@ -0,0 +1,43 @@
+class Item < ApplicationRecord
+  with_options presence: true do
+    validates :item_name
+    validates :item_description
+    validates :category_id
+    validates :status_id
+    validates :item_price
+    validates :burden_id
+    validates :source_id
+    validates :guideline_id
+  end
+
+  validates :item_price,
+    numericality: {
+    greater_than_or_equal_to: 300,
+    less_than: 10000000
+    }
+
+  validates :item_price, format: {with: /\A^[0-9]+$\z/, message: "半角数字のみで入力してください"}
+    
+  belongs_to :user
+  has_many :comments
+  has_one :solid_item
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :burden
+  belongs_to :source
+  belongs_to :guideline
+
+  validates :category_id, numericality: { other_than: 1 }
+
+  validates :status_id, numericality: { other_than: 1 }
+
+  validates :burden_id, numericality: {other_than: 1 }
+
+  validates :source_id, numericality: { other_than: 1 }
+
+  validates :guideline_id, numericality: { other_than: 1 }
+
+end
