da37e0205bf6374bf5cbe55b21d93cfe4d2a5e52
0405daichi <daichi.fukunaga.45@gmail.com>
2021-05-09 20:33:48 +0900

    商品削除機能　修正5

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
-  before_action :find_item, only: [:show, :edit, :update]
+  before_action :find_item, only: [:show, :edit, :update, :destroy]
   before_action :redirect_index, only: [:edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,8 +35,7 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    if item.destroy
+    if @item.destroy
       redirect_to action: :index
     end
   end
