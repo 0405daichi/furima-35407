7ff4f6e145e42af16d7e9e2420260d632082ec26
0405daichi <daichi.fukunaga.45@gmail.com>
2021-05-09 17:33:20 +0900

    商品削除機能　修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :find_item, only: [:show, :edit, :update]
+  before_action :redirect_index, only: [:edit, :update, :destroy]
   def index
     @items = Item.all.order('created_at DESC')
     @sold_items = SoldItem.all
@@ -19,22 +21,22 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-  # end
+  def edit
+  end
 
-  # def update
-  #   item = Item.find(params[:id])
-  #   item.update(item_params)
-  # end
+  def update
+    if @item.update(item_params)
+      redirect_to action: :index
+    else
+      render :edit
+    end
+  end
 
   def destroy
     item = Item.find(params[:id])
     item.destroy
-    redirect_to root_path
   end
 
   private
@@ -42,5 +44,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :item_description, :category_id, :status_id, :item_price, :burden_id, :source_id,
                                  :guideline_id, :image).merge(user_id: current_user.id)
+  end
+
+  def find_item
+    @item = Item.find(params[:id])
+  end
+
+  def redirect_index
+    if current_user != @item.user
+      redirect_to root_path
+    end
   end
 end
